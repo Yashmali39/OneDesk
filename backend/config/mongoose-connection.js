958fdb25e4fa3d1501d9f37ebc43c8f45913f6c8
Yash Ravindra Mali <yashrmali3@gmail.com>
2026-03-17 07:52:44 +0530

    final version

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dbgr = require('debug')("development:mongoose");
 const config = require('config');
 const connectDB = ()=>{
-    return mongoose.connect(`${config.get("MONGODB_URI")}/my-project`)
+    return mongoose.connect(`${config.get("MONGODB_URI")}/onedesk`)
                     .then(()=>{
                         dbgr("connected");
                     })
